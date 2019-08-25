018dc772f45dcccd43ccaba347aec5f5d91db71a
tom-foster <19tfoster87@gmail.com>
2019-08-25 22:11:44 +0100

    updated the spider so that it writes out all visited pages web links as well. This should mean in the future that it will be possible to then visit all the visited pages and push them to a link

--- wcc/spiders/wcc_spider.py
+++ wcc/spiders/wcc_spider.py
@@ -14,13 +14,25 @@ class WarwickshireSpider(scrapy.Spider):
 
     # extend the class
     def __init__(self):
-        super(scrapy.Spider, self).__init__()
+        super().__init__()
         self.pattern = re.compile('^https://www.warwickshire.gov.uk')
         self.visited_pages = []
         # not yet implemented
         self.visited_pages_all_links = {}
+        self.count = 0
 
     def parse(self, response):
+        page = response.url
+        if page not in self.visited_pages_all_links.keys():
+            self.visited_pages_all_links[page] = response.css('a::attr(href)').getall()
+        filename = 'visited_pages_all_links.txt'
+        with open(filename, 'w') as f:
+            for k, v in self.visited_pages_all_links.items():
+                f.write(k + '\n')
+                for x in range(len(v)):
+                    f.write('\t' + v[x] + '\n')
+                # f.write(str(v) + '\n\n')
+            
         for weblink in response.css("a::attr(href)").getall():
             yield {
                 'weblink' : weblink,
@@ -29,7 +41,6 @@ class WarwickshireSpider(scrapy.Spider):
             ## TODO
             if self.pattern.search(weblink):
                 yield scrapy.Request(response.urljoin(weblink), callback=self.parse)
-                page = response.url
                 if page not in self.visited_pages:
                     self.visited_pages.append(page)
                 filename = 'visited_pages_only_wcc.txt'
